2b0c72b206e764fcd6dc53d18d3dfbc45b3925e6
Ismael Caballero <icaballerohernandez@gmail.com>
2015-05-06 16:56:59 +0200

    Designe

---
+++ Views/indexNoLogin.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link href="./css/style.css" rel="stylesheet" type="text/css" />
+        <title></title>
+    </head>
+    <body>
+        <div class="wrapper">
+            <div class="upperMenu">
+                <div class="divMenuButton">
+                    <div class="buttonCategory">
+
+                    </div>
+                    <div class="buttonCategory">
+
+                    </div>
+                    <div class="buttonCategory">
+
+                    </div>
+                    <div class="buttonCategory">
+
+                    </div>
+                    <div class="buttonCategory">
+
+                    </div>
+                    <div class="buttonCategory">
+
+                    </div>
+                </div>
+                <div class="divMenuSearch">
+                    
+                </div>
+            </div>
+            <div class="middleWrapper">
+                <div class="innerMiddleWrapperLeft">
+                </div>
+                <div class="innerMiddleWrapperRight">
+                </div>
+            </div>
+            <div class="footerWrapper">
+                <div class="innerFooterWrapperLeft">
+
+                </div>
+                <div class="innerFooterWrapperMid">
+
+                </div>
+                <div class="innerFooterWrapperRight">
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
