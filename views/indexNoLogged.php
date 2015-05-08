eb3b7a2b4cdb5e0e5d08dd9908fff1dfa74262ac
adrian <adrian@adrian-X55VDR>
2015-05-08 17:01:36 +0200

    Basic default page without php

--- views/indexNoLogged.php
+++ views/indexNoLogged.php
@@ -31,7 +31,37 @@
         </div>
         <div class="middleWrapper">
             <p>
-                aaaa
+                One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin.
+            </p>
+            <p>
+                He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections.
+            </p>
+            <p>
+                The bedding was hardly able to cover it and seemed ready to slide off any moment.
+            </p>
+            <p>
+                His many legs, pitifully thin compared with the size of the rest of him, waved about helplessly as he looked.
+            </p>
+            <p>
+                "What's happened to me?" he thought.
+            </p>
+            <p>
+                It wasn't a dream.
+            </p>
+            <p>
+                His room, a proper human room although a little too small, lay peacefully between its four familiar walls.
+            </p>
+            <p>
+                A collection of textile samples lay spread out on the table - Samsa was a travelling salesman - and above it there hung a picture that he had recently cut out of an illustrated magazine and housed in a nice, gilded frame.
+            </p>
+            <p>
+                It showed a lady fitted out with a fur hat and fur boa who sat upright, raising a heavy fur muff that covered the whole of her lower arm towards the viewer.
+            </p>
+            <p>
+                Gregor then turned to look out the window at the dull weather.
+            </p>
+            <p>
+                Drops
             </p>
         </div>
         <div class="footerWrapper">
@@ -43,7 +73,7 @@
                 </ul>
             </div>
             <div class="footerMid innerFooter">
-                
+                <img src="./images/logoWithLetters.png" />
             </div>
             <div class="footerRight innerFooter">
                 <p>
