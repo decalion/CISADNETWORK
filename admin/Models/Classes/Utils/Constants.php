047a3e313fc23dacc5a17cdf9e6225440905f015
Ismael Caballero <icaballerohernandez@gmail.com>
2015-05-15 18:12:27 +0200

    adminpanel 0.1.1

--- admin/Models/Classes/Utils/Constants.php
+++ admin/Models/Classes/Utils/Constants.php
@@ -6,12 +6,12 @@ const PASS="icaballero23";
 const DATABASE="cisadnetwork";
   
 
-
-
+const BACK=1;
+const LOGOUT=10;
 const AUTHENTICATION=100;
 const USERPANEL=101;
 const USERMODIFY=102;
-const LOGOUT=10;
+
 
 
 
