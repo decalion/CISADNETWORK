05cb56008defa59c1a72f27a38aa5be0f869b6be
cristian bautista peral <cristianbautista55@gmail.com>
2015-05-18 21:38:30 +0200

    Merge origin/master
    Conflicts:
    	models/functions.php

--- models/functions.php
+++ models/functions.php
@@ -1,52 +1,5 @@
 <?php
 
-include './models/classes/Factory.php';
-
-    function loadDefault($type, $link) {
-        $factory = new Factory($type, $link);
-        $factory->loadDefaultInfo();
-    }
-    
-    function loadDetail($type, $link) {
-        $factory = new Factory($type, $link);
-        echo $_GET['type']." ".$_GET['id'];
-    }
-    
-    function encrypt($password) {
-        return sha1($password);
-    }
-    
-    function getDataByType($link, $type, $string) {
-        $query = 'select * from '.$type.' like title = '.$string;
-    }
-    
-?>
-<?php
-
-    include './models/classes/Factory.php';
-
-    function loadDefault($type, $link) {
-        $factory = new Factory($type, $link);
-        $factory->loadDefaultInfo();
-    }
-
-    function encrypt($password) {
-        return sha1($password);
-    }
-
-    function getDataByType($link, $type) {
-        $rows = array();
-        $query = 'select name from ' . $type ;// ' where name like "' . $string . '";';
-        $result = $link->query($query);
-        while ($row = mysqli_fetch_array($result)) {
-            array_push($rows, $row);
-        }
-        return $rows;
-    }
-
-?>
-<?php
-
     include './models/classes/Factory.php';
 
     function loadDefault($type, $link) {
@@ -63,8 +16,14 @@ include './models/classes/Factory.php';
         return sha1($password);
     }
     
-    function getDataByType($link, $type, $string) {
-        $query = 'select * from '.$type.' like title = '.$string;
+    function getDataByType($link, $type) {
+        $rows = array();
+        $query = 'select name from ' . $type ;// ' where name like "' . $string . '";';
+        $result = $link->query($query);
+        while ($row = mysqli_fetch_array($result)) {
+            array_push($rows, $row);
+        }
+        return $rows;
     }
     
 ?>
