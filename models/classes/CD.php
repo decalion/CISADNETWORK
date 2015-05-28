b72e99a3321f5894a47a9c3cc7e21b09b05a9f00
adrian <adrian@adrian-X55VDR>
2015-05-28 16:34:41 +0200

    Groups working

--- models/classes/CD.php
+++ models/classes/CD.php
@@ -7,6 +7,9 @@
         private $name;
         private $imageurl;
         
+        private $group;
+        private $songs;
+        
         function __construct($idcds, $idgroups, $name, $imageurl) {
             $this->idcds = $idcds;
             $this->idgroups = $idgroups;
@@ -34,6 +37,14 @@
         function getImageurl() {
             return $this->imageurl;
         }
+        
+        function getGroup($field) {
+            return $this->group[$field];
+        }
+        
+        function getSongs() {
+            return $this->songs;
+        }
 
         function setIdcds($idcds) {
             $this->idcds = $idcds;
@@ -49,6 +60,35 @@
 
         function setImageurl($imageurl) {
             $this->imageurl = $imageurl;
+        }
+        
+        function loadInfo($link) {
+            $this->setGroup($link);
+            $this->setSongs($link);
+        }
+        
+        function setGroup($link) {
+            $query = 'select idgroups, name from groups where groups.idgroups = '.$this->idgroups.';';
+            $groups = $link->query($query);
+            if ($groups->num_rows > 0) {
+                foreach ($groups as $group) {
+                    $this->group = array('idgroups' => $group['idgroups'], 'name' => $group['name']);
+                }
+            } else {
+                $this->group[] = "This cd don't have any group yet!";
+            }
+        }
+        
+        function setSongs($link) {
+            $query = 'select name from songs where idcds = '.$this->idcds.';';
+            $songs = $link->query($query);
+            if ($songs->num_rows > 0) {
+                foreach ($songs as $song) {
+                    $this->songs[] = '<p>'.$song['name'].'</p>';
+                }
+            } else {
+                $this->songs[] = "This cd don't have any song yet!";
+            }
         }
         
     }
